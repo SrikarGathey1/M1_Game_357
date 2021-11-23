2388f5d65b94aea26ea67a44b7e23d652d7020be
SrikarGathey1 <venkatnaras123@gmail.com>
2021-11-23 21:59:36 +0530

    comments for display.c added

--- 3_Implementation/src/display.c
+++ 3_Implementation/src/display.c
@@ -1,3 +1,29 @@
+/**
+ * @file display.c
+ * Used to display the grid diagrammatically during different valid states.
+ * @author Srikar Gade
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * The function takes in valid state values and output the corresponding state diagrams.
+ * 
+ * @param three used to store the number of circles to be printed in the third grid.(valid values range from 0 to 3)
+ * @param three_top used for printing the top row of the third grid.
+ * @param three_bottom used for printing the bottom row of the third grid.
+ * @param five used to store the number of circles to be printed in the fifth grid.(valid values range from 0 to 5)
+ * @param five_top used for printing the top row of the fifth grid.
+ * @param five_bottom used for printing the bottom row of the fifth grid.
+ * @param seven used to store the number of circles to be printed in the seven grid. (valid values range from 0 to 7)
+ * @param seven_top used for printing the top row of the seventh grid
+ * @param seven_bottom used for printing the bottom row of the seventh grid 
+ */
+
 void display_circle(int three, int five, int seven)
 {
     char *three_top = "O O";
@@ -71,6 +97,10 @@ void display_circle(int three, int five, int seven)
     printf("\t%s\t %s \t%s\n\n", three_bottom, five_bottom, seven_bottom);
 }
 
+/**
+ * @brief to handle invalid values.
+ * 
+ */
 void invalid()
 {
     printf("Enter a valid value");
